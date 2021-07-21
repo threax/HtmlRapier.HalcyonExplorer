e696083884c31b64a48f2b8db8e54394f39082e5
Andrew Piper <andrew@threax.com>
2021-07-21 15:07:55 -0400

    Add push script.

---
+++ Push.ps1
@@ -0,0 +1,8 @@
+git push
+git push --tags
+git push https://github.com/threax/HtmlRapier.HalcyonExplorer.git
+git push https://github.com/threax/HtmlRapier.HalcyonExplorer.git --tags
+git push https://gitlab.com/threax-software/HtmlRapier.HalcyonExplorer.git
+git push https://gitlab.com/threax-software/HtmlRapier.HalcyonExplorer.git --tags
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/HtmlRapier.HalcyonExplorer
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/HtmlRapier.HalcyonExplorer --tags
